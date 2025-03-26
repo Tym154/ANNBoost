6da16414a15fb82fb349e4d26fe56f64fff016f5
Tym154 <159148113+Tym154@users.noreply.github.com>
2025-03-26 10:46:45 +0100

    This doesn't have to be here anymore

--- ANN_lib/CORE_ANNBoost/include/core_serialization.hpp
+++ ANN_lib/CORE_ANNBoost/include/core_serialization.hpp
@@ -2,7 +2,6 @@
 #define CORE_SERIALIZATION_HPP
 
 #include "core_network.hpp"
-#include <fstream>
 
 // Saving a neural network to a txt file
 void save_current_network_to_file(const std::string &name_of_saved_file);
